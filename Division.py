ed5b318b13449405f1a18eab14f2fe0e464225aa
unknown <calebaguilera93@gmail.com>
2024-12-16 20:10:54 -0400

    bootcamp

---
+++ Division.py
@@ -0,0 +1,22 @@
+a = 5 
+b = 10 
+division  = a / b 
+print("La division es", division)
+
+A = int(input("Ingrese el número de A: "))
+B = int(input("Ingrese el número de B: "))
+division = A / B
+print(division)
+
+print(int(input("Ingrese A: ")) / int(input("Ingrese B: ")))
+
+def calcular_la_division(a, b):
+    return a / b
+
+d = calcular_la_division(2, 4)  
+print(d)
+
+a = int(input("Ingrese A: "))
+b = int(input("Ingrese B: "))
+d = calcular_la_division(a, b)  
+print(d)
---
+++ multiplicaciones.py
@@ -0,0 +1,22 @@
+a = 5 
+b = 10 
+multiplicación = a * b 
+print("La multiplicación es", multiplicación)
+
+A = int(input("Ingrese el número de A: "))
+B = int(input("Ingrese el número de B: "))
+multiplicación = A * B
+print(multiplicación)
+
+print(int(input("Ingrese A: ")) * int(input("Ingrese B: ")))
+
+def calcular_la_multiplicación(a, b):
+    return a * b
+
+m = calcular_la_multiplicación(2, 4)  
+print(m)
+
+a = int(input("Ingrese A: "))
+b = int(input("Ingrese B: "))
+m = calcular_la_multiplicación(a, b)  
+print(m)
