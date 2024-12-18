670169c54f4885c23e9f4a7227642dad9f19363d
ardillafat <calebaguilera93@gmail.com>
2024-12-18 15:09:56 -0400

    Add files via upload

---
+++ conversion mondas.py
@@ -0,0 +1,22 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    C o n s t a n t e    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# dollar = int(input("ingrese e monto el dolares: \n"))
+# Tipo_de_cambio = float(input("Ingrese el tipo de cambio: \n"))
+# bolivianos = dollar*Tipo_de_cambio
+# print(bolivianos)
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    V a r i a b l e    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+Dollar = float(input("Monto en dollares: "))
+Tipo_de_cambio = 10.00
+Conversion = Dollar * Tipo_de_cambio
+print("el monto de cambio es: ", Conversion)
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    S o l a   L i n e a    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# print(float(input("Dollar: ")) * float(input("Bolivian: ")))
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%    F u n c i ó n    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# def Cambio(Dollar, bolivian):
+#     return Dollar*bolivian
+# Dollar = float(input("Ingrese el monto en dolares: "))
+# Bolivian = float(input("Ingrese el monto en bolivianos: "))
+# Resultado = Cambio(Dollar,Bolivian)
+# print(Resultado)
+
